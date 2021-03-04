5ce398591cbd7b151389593d0b7c4968adfdd470
KarimGalliciotti <61277897+KarimGalliciotti@users.noreply.github.com>
2021-03-04 14:53:09 +0100

    Add files via upload

---
+++ src/Assets/Scripts/PixelConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelConverter : MonoBehaviour
+{
+    public static float UnityToPixels(int unityValue)
+    {
+        return (float)unityValue / 100;
+    }
+}
