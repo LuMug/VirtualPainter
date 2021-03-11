568d8d9c4797b13438851d6f785d9d8f96a465c3
KarimGalliciotti <61277897+KarimGalliciotti@users.noreply.github.com>
2021-03-11 08:37:54 +0100

    Add files via upload

---
+++ src/Assets/Scripts/Close.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Close : MonoBehaviour
+{
+    private FileManager file;
+
+    public void salvaEdEsci()
+    {
+        string json = File.ReadAllText("Assets/Models/paths.json");
+        List<Paths> oldPaths = JsonConvert.DeserializeObject<List<Paths>>(json);
+        string extension = oldPaths[oldPaths.Count - 1].path.Substring(oldPaths.Count - 3, 3);
+        file = new FileManager(oldPaths[oldPaths.Count - 1].path);
+        var texture = file.GetTexture();
+        if (extension.Equals("png"))
+        {
+            file.SaveTexturePNG(texture);
+        }
+        else if (extension.Equals("jpg"))
+        {
+            file.SaveTextureJPG(texture);
+        }
+        Application.Quit();
+    }
+    public void esci()
+    {
+        Application.Quit();
+    }
+}
